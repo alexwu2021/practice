aa5b80dd13399c93ade9db79175c72b1834365f3
alexwu2021 <alexwu2021@gmail.com>
2019-04-09 23:11:10 -0700

    more works

---
+++ Java/src/Medium/WordBreak.java
@@ -0,0 +1,32 @@
+package Medium;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+
+    //    --->
+    //    |
+    //    |
+    //
+
+    /** passed oj
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreak_submission1(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] f = new boolean[n + 1];
+        Set<String> st = new HashSet<>(wordDict);
+        f[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                f[i] =  f[j] && st.contains(s.substring(j, i));
+                if (f[i]) break;
+            }
+        }
+        return f[n];
+    }
+}
