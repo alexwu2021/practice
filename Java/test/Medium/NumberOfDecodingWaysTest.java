c7bd2b8e4e9984dc0fb7b74cc677a5c7cb50eab9
alexwu2021 <alexwu2021@gmail.com>
2019-04-30 23:24:37 -0700

    more works

---
+++ Java/test/Medium/NumberOfDecodingWaysTest.java
@@ -0,0 +1,29 @@
+package Medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumberOfDecodingWaysTest {
+
+    @Test
+    public void numDecodings() {
+
+        NumberOfDecodingWays numberOfDecodingWays = new NumberOfDecodingWays();
+        String input;
+        int actual, expected;
+
+//        input = "226";
+//        expected = 3;
+//
+//        actual = numberOfDecodingWays.numDecodings(input);
+//        assert(actual == expected);
+//
+
+        input = "22";
+        expected = 2;
+
+        actual = numberOfDecodingWays.numDecodings(input);
+        assert(actual == expected);
+    }
+}
