3502a7f8b0e4587b2f891b8c09afd08cdba8ef34
Alex Wu <awu@cardeabio.com>
2020-11-22 10:57:52 -0800

    solved one problem

--- Java/src/amazon/PairsOfSongs.java
+++ Java/src/amazon/PairsOfSongs.java
@@ -4,7 +4,43 @@ import java.util.*;
 
 public class PairsOfSongs {
 
-    public int getNumberOfPairs(int[] times){
+    public int numPairsDivisibleBy60_smartie(int[] time) {
+        int c[]  = new int[60], res = 0;
+        for (int t : time) {
+            res += c[(600 - t) % 60];
+            c[t % 60] += 1;
+        }
+        return res;
+    }
+
+    public int numPairsDivisibleBy60(int[] time) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int cntr = 0;
+        for (int t : time) {
+            cntr += map.getOrDefault((60 - t % 60) % 60, 0);
+            map.put(t % 60, map.getOrDefault(t % 60, 0) + 1);
+        }
+        return cntr;
+    }
+
+
+    /** workable
+     * @param times
+     * @return
+     */
+    public int numPairsDivisibleBy60_2(int[] times) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int cntr = 0;
+        for (int t : times) {
+            int proper = t % 60;
+            int cmp = (60 - proper) % 60;
+            cntr += mp.getOrDefault(cmp, 0);
+            mp.put(proper, mp.getOrDefault(proper, 0) + 1);
+        }
+        return cntr;
+    }
+
+        public int getNumberOfPairs(int[] times){
         Map<Integer, List<Integer>> valToIndices = new HashMap<>();
         for(int i = 0; i < times.length; ++i){
             valToIndices.computeIfAbsent(times[i], x ->new ArrayList<Integer>()).add(i);
