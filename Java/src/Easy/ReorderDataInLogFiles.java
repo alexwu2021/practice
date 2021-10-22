34308e66a329c42d2ab2821bf70bf9daf6cf5b4a
Alex Wu <awu@cardeabio.com>
2021-10-22 15:37:47 -0700

    anagram sentence

--- Java/src/Easy/ReorderDataInLogFiles.java
+++ Java/src/Easy/ReorderDataInLogFiles.java
@@ -1,9 +1,6 @@
 package Easy;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
@@ -22,6 +19,40 @@ import java.util.Queue;
  *
  */
 public class ReorderDataInLogFiles {
+
+    /**
+     * from browntiger
+     * @param logs
+     * @return
+     */
+    public String[] reorderLogFiles_preferred(String[] logs) {
+        Arrays.sort(logs, (s1, s2) -> {
+            String[] split1 = s1.split(" ", 2);
+            String[] split2 = s2.split(" ", 2);
+
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+
+            if(!isDigit1 && !isDigit2) {
+                // both letter-logs.
+                int comp = split1[1].compareTo(split2[1]);
+                if (comp == 0) return split1[0].compareTo(split2[0]);
+                else return comp;
+            } else if (isDigit1 && isDigit2) {
+                // both digit-logs. So keep them in original order
+                return 0;
+            } else if (isDigit1 && !isDigit2) {
+                // first is digit, second is letter. bring letter to forward.
+                return 1;
+            } else {
+                //first is letter, second is digit. keep them in this order.
+                return -1;
+            }
+        });
+        return logs;
+    }
+
+
     public String[] reorderLogFiles(String[] logs) {
         List<String> ans = new ArrayList<>();
         if(logs == null || logs.length <= 0) return ans.toArray(new String[0]);
