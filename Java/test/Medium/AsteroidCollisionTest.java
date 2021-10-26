7cb8a2df286530263e68d8e584687cb3e45ce93e
Alex Wu <awu@cardeabio.com>
2021-10-25 22:06:56 -0700

    added surrounding region, correct bu tle

---
+++ Java/test/Medium/AsteroidCollisionTest.java
@@ -0,0 +1,20 @@
+package Medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AsteroidCollisionTest {
+
+    @Test
+    public void asteroidCollision() {
+        AsteroidCollision sol = new AsteroidCollision();
+        int[] a, actual, expected;
+
+        a = new int[]{10,2,-5};
+        expected = new int[]{10};
+        actual = sol.asteroidCollision(a);
+        Assert.assertArrayEquals(expected, actual);
+    }
+}
