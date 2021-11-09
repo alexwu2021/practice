0cc758c511ddffe90b3941c0f5334a46a7420a96
Alex Wu <awu@cardeabio.com>
2021-11-08 18:24:09 -0800

    added count good triplets, decrypt string from alphabet to integer mapping, max 69 numbers

---
+++ Java/src/Easy/CountGoodTriplets.java
@@ -0,0 +1,66 @@
+package Easy;
+
+/**
+ * 1534. Count Good Triplets
+ * Easy
+ *
+ * 341
+ *
+ * 770
+ *
+ * Add to List
+ *
+ * Share
+ * Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+ *
+ * A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+ *
+ * 0 <= i < j < k < arr.length
+ * |arr[i] - arr[j]| <= a
+ * |arr[j] - arr[k]| <= b
+ * |arr[i] - arr[k]| <= c
+ * Where |x| denotes the absolute value of x.
+ *
+ * Return the number of good triplets.
+ */
+public class CountGoodTriplets {
+
+    public int countGoodTriplets_bruteForce(int[] arr, int a, int b, int c) {
+        int ans = 0;
+        int n = arr.length;
+        for(int i=0; i<n-2; ++i){
+            for(int j=i+1; j<n-1; ++j){
+                for(int k=j+1; k<n; ++k){
+                    if(Math.abs(arr[i] - arr[j]) > a) continue;
+                    if(Math.abs(arr[j] - arr[k]) > b) continue;
+                    if(Math.abs(arr[i] - arr[k]) > c) continue;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 10 ms, faster than 95.91% of Java online submissions for Count Good Triplets.
+     * @param arr
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int res = 0, n = arr.length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) > a) continue;
+                for (int k = j + 1; k < n; k++) {
+                    if (Math.abs(arr[j] - arr[k]) > b) continue;
+                    if (Math.abs(arr[k] - arr[i]) > c) continue;
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ Java/test/Easy/DecryptStringFromAlphabet2IntegerMappingTest.java
@@ -0,0 +1,47 @@
+package Easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DecryptStringFromAlphabet2IntegerMappingTest {
+
+    @Test
+    public void freqAlphabets() {
+        DecryptStringFromAlphabet2IntegerMapping sol = new DecryptStringFromAlphabet2IntegerMapping();
+        String s, actual, expected;
+
+        //Input: s = "10#11#12"
+        //Output: "jkab"
+        //Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+        s = "10#11#12";
+        expected = "jkab";
+        actual = sol.freqAlphabets(s);
+        Assert.assertEquals(expected, actual);
+
+        //Input: s = "1326#"
+        //Output: "acz"
+        s = "1326#";
+        expected = "acz";
+        actual = sol.freqAlphabets(s);
+        Assert.assertEquals(expected, actual);
+
+
+        //Input: s = "25#"
+        //Output: "y"
+        s = "25#";
+        expected = "y";
+        actual = sol.freqAlphabets(s);
+        Assert.assertEquals(expected, actual);
+
+        //Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+        //Output: "abcdefghijklmnopqrstuvwxyz"
+        s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#";
+        expected = "abcdefghijklmnopqrstuvwxyz";
+        actual = sol.freqAlphabets(s);
+        Assert.assertEquals(expected, actual);
+
+
+    }
+}
